3f7033f17f006c3889d4a92cec9944f76c73201c
zeus-ff <tofaniyam@gmail.com>
2016-04-14 20:25:11 +0100

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {our_id},
+    sudo_users = {184405202},
     admin_users = {},
     disabled_channels = {}
   }
